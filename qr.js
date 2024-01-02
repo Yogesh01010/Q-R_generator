744ed5ba6f0b5ee4e3e8b9a0144b7a3ab26000f7
Yogesh01010 <112406803+Yogesh01010@users.noreply.github.com>
2024-01-02 15:42:29 +0530

    Update qr.js

--- qr.js
+++ qr.js
@@ -15,7 +15,7 @@ inquirer
     qr_svg.pipe(fs.createWriteStream("qr_img.png"));
 
     fs.writeFile("URL.txt", url, (err) => {
-      if (err) throw err; 
+      if (err) throw err;
       console.log("The file has been saved!");
     });
   })
